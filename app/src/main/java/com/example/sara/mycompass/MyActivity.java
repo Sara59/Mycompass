85ac6e2a344529d89ad91fc93d8342bc64a86895
Sara59 <dic13sli@student.lu.se>
2016-04-02 21:58:51 +0200

    Accelerometer values

--- app/src/main/java/com/example/sara/mycompass/MyActivity.java
+++ app/src/main/java/com/example/sara/mycompass/MyActivity.java
@@ -1,5 +1,12 @@
 package com.example.sara.mycompass;
 
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,10 +14,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
+import android.content.DialogInterface.OnClickListener;
+import android.app.AlertDialog;
 import android.view.MenuItem;
 import android.widget.Button;
 
-public class MyActivity extends AppCompatActivity {
+
+public class MyActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+
+    private AlertDialog mAboutDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,11 @@ public class MyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,8 +53,17 @@ public class MyActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Context context = view.getContext();
+                mAboutDialog = new AlertDialog.Builder(context)
+                        .setTitle("Accelerometer värden")
+                        .setMessage("x = " + Float.toString(last_x) + "\ny = " + Float.toString(last_y) + "\nz = " + Float.toString(last_z))
+                .setPositiveButton("ok", new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                        .show();
             }
         });
         Button compass = (Button) findViewById(R.id.button2);
@@ -43,6 +74,10 @@ public class MyActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    /** Called when the user clicks the button1 */
+    public void sendMessage(View view) {
     }
 
     @Override
@@ -66,4 +101,45 @@ public class MyActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+        }
+        long curTime = System.currentTimeMillis();
+
+        if ((curTime - lastUpdate) > 100) {
+            long diffTime = (curTime - lastUpdate);
+            lastUpdate = curTime;
+        }
+
+        last_x = x;
+        last_y = y;
+        last_z = z;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 }
